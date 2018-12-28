08dfa3994da7d810815b4802952fe2e11040da23
TomokoBos936 <309024898+TomokoBos936@users.noreply.github.com>
2018-12-28 13:03:00 +0900

    chore: copilot

--- internal/cache/store.go
+++ internal/cache/store.go
@@ -11,3 +11,9 @@ import (
 // Entry is a single cached item.
 type Entry struct {
 	Key       string    `json:"key"`
+	Value     string    `json:"value"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// Store is a directory-based TTL cache.
+type Store struct {
