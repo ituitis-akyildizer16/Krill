5a7e0ac00cbf42a8e218adf214c57d2bbc6193f8
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-05-10 16:45:00 +0900

    build: local-first

--- internal/git/blame.go
+++ internal/git/blame.go
@@ -11,3 +11,8 @@ type BlameLine struct {
 	Commit    string
 	Author    string
 	Timestamp string
+	LineNo    int
+	Content   string
+}
+
+// Blame returns up to maxLines lines of porcelain blame for a path,
