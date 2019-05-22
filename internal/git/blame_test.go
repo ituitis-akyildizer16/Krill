bc6211bb52157d16059fddc231d71b9d78b689c8
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-05-22 14:38:00 +0900

    chore: cli

--- internal/git/blame_test.go
+++ internal/git/blame_test.go
@@ -5,3 +5,6 @@ import "testing"
 func TestParseBlame(t *testing.T) {
 	out := `4c8a1f3a2b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e 1 1
 author Alice
+author-mail <alice@example.com>
+author-time 1700000000
+author-tz +0000
