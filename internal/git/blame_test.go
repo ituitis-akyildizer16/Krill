ab2cbf4f4ffb8f10edc7e880b2ceb1cbdd226be6
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-05-31 15:24:00 +0900

    docs: document developer-tools

--- internal/git/blame_test.go
+++ internal/git/blame_test.go
@@ -26,3 +26,6 @@ author-tz +0000
 	}
 	if lines[0].Commit[:8] != "4c8a1f3a" {
 		t.Errorf("first commit = %q", lines[0].Commit)
+	}
+	if lines[0].Author != "Alice" {
+		t.Errorf("first author = %q", lines[0].Author)
