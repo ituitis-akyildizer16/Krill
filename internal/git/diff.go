bb1d6c9dc92f26c4ad8ecc332ef0c2d555a128b9
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-07-08 11:15:00 +0900

    style: clean up git

--- internal/git/diff.go
+++ internal/git/diff.go
@@ -8,3 +8,6 @@ import (
 
 // Diff returns the uncommitted diff (staged or unstaged) bounded by
 // maxLines of output.
+func (r *Runner) Diff(ctx context.Context, staged bool, maxLines int) (string, error) {
+	args := []string{"diff"}
+	if staged {
