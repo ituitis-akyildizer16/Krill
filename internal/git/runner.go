d69fd59eb3289e3828c658d0fa8fa5b1bd46e3dc
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-08-19 18:36:00 +0900

    test: cover digest

---
+++ internal/git/runner.go
@@ -0,0 +1,6 @@
+// Package git wraps the git CLI for context collection.
+//
+// Everything here shells out to `git` (the source of truth); we only
+// parse its output. Commands are bounded by context, never by timeouts
+// that would corrupt the output.
+package git
