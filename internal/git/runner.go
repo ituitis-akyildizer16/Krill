da24ba60990a5123dff18be8b35e65d660162319
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2019-09-18 16:03:00 +0900

    fix: cli

--- internal/git/runner.go
+++ internal/git/runner.go
@@ -33,3 +33,6 @@ func (r *Runner) Dir() string { return r.dir }
 func (r *Runner) Exec(ctx context.Context, args ...string) (string, error) {
 	cmd := exec.CommandContext(ctx, "git", append([]string{"-C", r.dir}, args...)...)
 	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
