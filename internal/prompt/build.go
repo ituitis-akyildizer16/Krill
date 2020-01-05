a0ac8d9c143d0d82347ba4aaba72a6999c3d656f
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2020-01-05 13:27:00 +0900

    perf: improve cli

--- internal/prompt/build.go
+++ internal/prompt/build.go
@@ -9,3 +9,7 @@ import (
 // System is the base system prompt for coding questions.
 const System = `You are krill, a local-first terminal copilot running on the
 user's machine with no network access to any model service.
+
+Rules:
+- Answer ONLY from the provided context (git blame, diff, log, file).
+- When you reference history, cite the short commit SHA.
