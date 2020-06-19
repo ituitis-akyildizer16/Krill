8279d3c068c4f46e1b0abc673e6bec83536bbf2a
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2020-06-19 13:03:00 +0900

    feat: add digest

--- cmd/krill/main.go
+++ cmd/krill/main.go
@@ -1,3 +1,5 @@
 // Command krill is a local-first terminal AI copilot.
 //
 // It collects git context (blame, diff, history), builds a bounded prompt,
+// and streams an answer from a local Ollama model. Nothing leaves the
+// machine; the VS Code extension (extension/) and plugin SDK (plugins/)
