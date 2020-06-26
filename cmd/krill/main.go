6401b3c1e72d9f0f2ad8305fd7aea931e6da937a
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2020-06-26 11:34:00 +0900

    refactor: tidy terminal

--- cmd/krill/main.go
+++ cmd/krill/main.go
@@ -3,3 +3,5 @@
 // It collects git context (blame, diff, history), builds a bounded prompt,
 // and streams an answer from a local Ollama model. Nothing leaves the
 // machine; the VS Code extension (extension/) and plugin SDK (plugins/)
+// talk to the same core over stdio/JSON-RPC.
+package main
