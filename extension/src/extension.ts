a6a4369b445421f7b5b2abfa8179f3ae7867dd5a
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2020-09-29 11:34:00 +0900

    chore: blame

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -6,3 +6,11 @@ const exec = promisify(execFile);
 
 function binary(): string {
   return vscode.workspace
+    .getConfiguration("krill")
+    .get<string>("binaryPath", "krill");
+}
+
+function runKrill(args: string[], input?: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const child = exec(binary(), args, { cwd: workspaceRoot() });
+    child.then(({ stdout }) => resolve(stdout.trim())).catch(reject);
