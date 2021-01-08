9543b238b019317636c8b932636902d3ad6f118a
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-01-08 11:49:00 +0900

    chore: copilot

--- plugins/sdk/krill_plugins/__init__.py
+++ plugins/sdk/krill_plugins/__init__.py
@@ -1,3 +1,4 @@
 """krill_plugins - Python SDK for krill plugin providers."""
 
 from krill_plugins.base import ContextProvider, SuggestPostprocessor
+from krill_plugins.registry import load_plugins, providers
