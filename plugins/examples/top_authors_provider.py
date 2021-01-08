f94b785d04499f3f132f4083160412aa6259aa8f
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-01-08 11:30:00 +0900

    style: clean up developer-tools

--- plugins/examples/top_authors_provider.py
+++ plugins/examples/top_authors_provider.py
@@ -6,3 +6,5 @@ from krill_plugins import ContextProvider
 class TopAuthorsProvider(ContextProvider):
     name = "top_authors"
 
+    def collect(self, repo: dict, file: str | None = None) -> str:
+        if not file:
