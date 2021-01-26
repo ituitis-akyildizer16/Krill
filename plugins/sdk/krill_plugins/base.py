f2d47019efcfe6eef74d9277e0f7b2d76478ff65
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-01-26 18:12:00 +0900

    feat: cli

--- plugins/sdk/krill_plugins/base.py
+++ plugins/sdk/krill_plugins/base.py
@@ -8,3 +8,9 @@ from typing import Any, Optional
 
 @dataclass
 class ContextProvider:
+    """Collects extra context that is appended to the prompt.
+
+    Subclasses set ``name`` and implement ``collect``. The core calls
+    ``collect`` with a 2-second deadline; exceptions are swallowed and
+    the provider's output is skipped.
+    """
