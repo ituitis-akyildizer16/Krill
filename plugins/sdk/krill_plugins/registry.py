954c0b7565f00ed7b1ae37a8324fd2606ac5bd0c
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-02-08 09:38:00 +0900

    docs: document digest

--- plugins/sdk/krill_plugins/registry.py
+++ plugins/sdk/krill_plugins/registry.py
@@ -11,3 +11,10 @@ from krill_plugins.base import ContextProvider, SuggestPostprocessor
 
 
 def _load_module(path: Path) -> Optional[object]:
+    spec = importlib.util.spec_from_file_location(
+        f"krill_plugin_{path.stem}", path
+    )
+    if spec is None or spec.loader is None:
+        return None
+    module = importlib.util.module_from_spec(spec)
+    try:
