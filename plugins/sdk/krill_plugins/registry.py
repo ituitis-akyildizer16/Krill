0ae6266f855e1d61a00a2e9d93141eba71acc2bc
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-02-08 09:20:00 +0900

    fix: correct git

--- plugins/sdk/krill_plugins/registry.py
+++ plugins/sdk/krill_plugins/registry.py
@@ -4,3 +4,10 @@ from __future__ import annotations
 
 import importlib.util
 import sys
+from pathlib import Path
+from typing import Optional
+
+from krill_plugins.base import ContextProvider, SuggestPostprocessor
+
+
+def _load_module(path: Path) -> Optional[object]:
