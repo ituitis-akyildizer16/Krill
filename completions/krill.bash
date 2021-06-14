c97dd5294d309541c48b547f354ff881f8ac5e67
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-06-14 15:33:00 +0900

    fix: cli

--- completions/krill.bash
+++ completions/krill.bash
@@ -1,3 +1,7 @@
 # krill bash completion
 # source this file: source completions/krill.bash
 
+_krill() {
+    local cur prev
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
