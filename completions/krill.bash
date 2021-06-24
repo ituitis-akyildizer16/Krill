0002860f9cb5316231aba0ca59e251536725b879
TomokoBos936 <309024898+TomokoBos936@users.noreply.github.com>
2021-06-24 14:34:00 +0900

    fix: terminal

--- completions/krill.bash
+++ completions/krill.bash
@@ -5,3 +5,7 @@ _krill() {
     local cur prev
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    local commands="ask suggest review status init models version"
+    local files="--file -f --context-only --inline --shell --staged"
+
