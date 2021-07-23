deac2ad7e7dd89e8c0b7cec20ef552ab5cac3436
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-07-23 10:12:00 +0900

    fix: correct git

--- completions/krill.fish
+++ completions/krill.fish
@@ -4,3 +4,5 @@
 complete -c krill -f
 
 complete -c krill -n "__fish_use_subcommand" -a ask -d "ask a question about the current repo"
+complete -c krill -n "__fish_use_subcommand" -a suggest -d "suggest a shell command"
+complete -c krill -n "__fish_use_subcommand" -a review -d "summarize the current diff"
