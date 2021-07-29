ba120068b354a25162575ff20d140e55785994cd
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-07-29 13:27:00 +0900

    build: ollama

--- completions/krill.ps1
+++ completions/krill.ps1
@@ -1,2 +1,6 @@
 # krill PowerShell completion
 # Add to $PROFILE: . ./completions/krill.ps1
+
+Register-ArgumentCompleter -Native -CommandName krill -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
