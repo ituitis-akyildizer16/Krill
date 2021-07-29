d88c9ba208a618c350031b04d0e04ac02755324e
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2021-07-29 13:45:00 +0900

    fix: correct cli

--- completions/krill.ps1
+++ completions/krill.ps1
@@ -4,3 +4,6 @@
 Register-ArgumentCompleter -Native -CommandName krill -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
 
+    $commands = @('ask', 'suggest', 'review', 'status', 'init', 'models', 'version')
+    $tokens = $commandAst.CommandElements | ForEach-Object { $_.Extent.Text }
+    $currentCommand = $tokens[1]
