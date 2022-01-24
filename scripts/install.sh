582886a04d7f2be3d29d210ef47566db1695dbcd
agnieszkabianchi729 <309024251+agnieszkabianchi729@users.noreply.github.com>
2022-01-24 13:28:00 +0900

    refactor: tidy digest

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 VERSION="${VERSION:-$(git describe --tags --always 2>/dev/null || echo dev)}"
 
 echo ">> building krill v${VERSION}"
+go build -ldflags "-X github.com/VGaussaleex/krill/internal/cli.Version=${VERSION}" \
+  -o "${GOBIN:-$(go env GOPATH)/bin}/krill" ./cmd/krill
