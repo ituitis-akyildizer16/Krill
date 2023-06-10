496446e531317abbb680200f8298e292b7bf0d24
TomokoBos936 <309024898+TomokoBos936@users.noreply.github.com>
2023-06-10 14:27:00 +0900

    fix: correct local-first

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,3 +7,6 @@ VERSION="${VERSION:-$(git describe --tags --always 2>/dev/null || echo dev)}"
 echo ">> building krill v${VERSION}"
 go build -ldflags "-X github.com/VGaussaleex/krill/internal/cli.Version=${VERSION}" \
   -o "${GOBIN:-$(go env GOPATH)/bin}/krill" ./cmd/krill
+
+echo ">> installed to $(go env GOPATH)/bin/krill"
+echo ">> next: ollama pull qwen2.5-coder:7b && krill status"
