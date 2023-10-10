9a637c4497f6ca28434439eae9c05ca49530905e
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2023-10-10 09:27:00 +0900

    build: ollama

--- plugins/examples/top_authors_provider.py
+++ plugins/examples/top_authors_provider.py
@@ -8,3 +8,6 @@ class TopAuthorsProvider(ContextProvider):
 
     def collect(self, repo: dict, file: str | None = None) -> str:
         if not file:
+            return ""
+        # In production this would shell out to `git shortlog -sne -- <file>`.
+        return "Recent authors for this file: see git shortlog."
