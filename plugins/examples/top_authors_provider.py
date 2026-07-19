416eca43a56354ac841103a9be4f22ed149ab4f2
ituitis-akyildizer16 <35951731+ituitis-akyildizer16@users.noreply.github.com>
2026-07-19 09:12:00 +0200

    chore: keep ignore rules current

--- plugins/examples/top_authors_provider.py
+++ plugins/examples/top_authors_provider.py
@@ -11,3 +11,4 @@ class TopAuthorsProvider(ContextProvider):
             return ""
         # In production this would shell out to `git shortlog -sne -- <file>`.
         return "Recent authors for this file: see git shortlog."
+# draft note 7
